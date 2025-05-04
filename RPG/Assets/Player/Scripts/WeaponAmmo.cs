35ea942c5f97a3b2f10b33b17f6534b48c00d152
MartaAlarconValdes <martaalarconvaldes@gmail.com>
2025-05-04 19:06:29 +0200

    reloading

--- RPG/Assets/Player/Scripts/WeaponAmmo.cs
+++ RPG/Assets/Player/Scripts/WeaponAmmo.cs
@@ -22,15 +22,23 @@ public class WeaponAmmo : MonoBehaviour
     {
         if (extraAmmo >= clipSize)
         {
-            int ammoToReload = clipSize - currentAmmo;  
-            extraAmmo -= ammoToReload;  
+            int ammoToReload = clipSize - currentAmmo;
+            extraAmmo -= ammoToReload;
             currentAmmo += ammoToReload;
         }
         else if (extraAmmo > 0)
         {
-            currentAmmo += extraAmmo;  
-            extraAmmo = 0;  
+            if (extraAmmo + currentAmmo > clipSize)
+            {
+                int leftOverAmmo = extraAmmo + currentAmmo - clipSize;
+                extraAmmo = leftOverAmmo;
+                currentAmmo = clipSize;
+            }
+            else
+            {
+                currentAmmo += extraAmmo;
+                extraAmmo = 0;
+            }
         }
     }
-
 }
